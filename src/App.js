bf984e7da16e47d3adb5f6fecfe08ef5680f15d6
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-11-13 16:35:48 +0900

    partially initializee routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignInPage from "./routes/SignInPage/SignInPage";
 import SignUpPage from "./routes/SignUpPage/SignUpPage";
 import UrlsPage from "./routes/UrlsPage/UrlsPage";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <SignInPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/projects" element={<UrlsPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
