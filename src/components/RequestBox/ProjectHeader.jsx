b6bcca6cddf63850fa3e9f0a1b4c0acb9655a80c
sehyeon7 <worldjade07@gmail.com>
2023-11-15 18:33:40 +0900

    create: ProjectHeader.jsx

---
+++ src/components/RequestBox/ProjectHeader.jsx
@@ -0,0 +1,13 @@
+import "./Index.css";
+
+const ProjectHeader = () => {
+    return(
+        <div className="ProjectHeader">
+            <div className="project_header_box">
+                <h1>http://localhost:3000</h1>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectHeader;
