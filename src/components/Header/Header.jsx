6c54f38a69f47d944323732a849ad8c572fb1361
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-11-17 11:27:01 +0900

    partially connect login api to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,17 @@
 import "./Index.css";
+import { useEffect, useState } from "react";
+import { getCookie, removeCookie } from "../../utils/cookie";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = getCookie("access_token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <div className="Header">
       <p>sign out</p>
