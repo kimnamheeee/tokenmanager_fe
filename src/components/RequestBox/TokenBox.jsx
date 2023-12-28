742df08175f5024a701c920d98593e867cf0ff78
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-12-28 18:10:44 +0900

    fix: request page ui

--- src/components/RequestBox/TokenBox.jsx
+++ src/components/RequestBox/TokenBox.jsx
@@ -1,9 +1,14 @@
 import "./Index.css";
 
 const TokenBox = () => {
-    return(
-        <div className="TokenBox">access token</div>
-    );
-}
+  return (
+    <div className="TokenBox">
+      <p className="token-type">access token</p>
+      <p className="token-content-box">
+        7dcSRCtmu00XDFxxwAXU3ty7aBhsGM9w3H3ps0jKKmBbTAHFZZAqMH6EagJBFtghQfPQHRzsmIqYRaDc7XDTql331Sgs1wkh87rfAjkEzLtEavR101D6GuRqHBMsVcQr
+      </p>
+    </div>
+  );
+};
 
 export default TokenBox;
