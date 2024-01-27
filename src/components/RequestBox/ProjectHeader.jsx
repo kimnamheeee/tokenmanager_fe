d0b307010ea609ca15e3e8386fead9e17244a2bb
sehyeon7 <worldjade07@gmail.com>
2024-01-27 17:49:54 +0900

    create: project header logic

--- src/components/RequestBox/ProjectHeader.jsx
+++ src/components/RequestBox/ProjectHeader.jsx
@@ -1,10 +1,31 @@
 import "./Index.css";
+import { useState, useEffect } from "react";
+import { getProjectDetail } from "../../api/api";
 
-const ProjectHeader = () => {
+const ProjectHeader = ({project}) => {
+  const [projectDetail, setProjectDetail]=useState();
+  console.log(project)
+
+  useEffect(() => {
+    console.log("USEEFFECTTTTT")
+    const getProjectDetailAPI = async (data) => {
+      const projects = await getProjectDetail(data);
+      setProjectDetail(projects);
+    };
+    getProjectDetailAPI(project);
+  }, []);
+
+  useEffect(()=>{
+    console.log("바뀜");
+    console.log(projectDetail);
+  }, [projectDetail])
+
+
+
   return (
     <div className="ProjectHeader">
       <div className="project-header-box">
-        <h1>http://localhost:3000</h1>
+        <h1>{projectDetail.url}</h1>
       </div>
     </div>
   );
