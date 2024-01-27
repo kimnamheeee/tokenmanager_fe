6f95b531c102e9803ddc83972d05f0a6b59d9d00
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2024-01-27 14:40:27 +0900

    fix: fix token time box structure

--- src/components/RequestBox/TokenTimeBox.jsx
+++ src/components/RequestBox/TokenTimeBox.jsx
@@ -3,7 +3,10 @@ import "./Index.css";
 const TokenTimeBox = () => {
   return (
     <div className="TokenTimeBox">
-      <div className="token-time-box"></div>
+      <div className="token-time-box">
+        <div className="token-name">accss_token</div>
+        <div className="token-duration">30:00</div>
+      </div>
     </div>
   );
 };
