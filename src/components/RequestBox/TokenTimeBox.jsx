cdd9f9140f66c865eba313e193f3d0a0d525efbf
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2024-01-27 19:44:49 +0900

    fix: token duration content (for test)

--- src/components/RequestBox/TokenTimeBox.jsx
+++ src/components/RequestBox/TokenTimeBox.jsx
@@ -3,18 +3,15 @@ import "./Index.css";
 const TokenTimeBox = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    
   };
 
-  const handleSelectChange = (e) => {
-    
-  };
+  const handleSelectChange = (e) => {};
 
   return (
     <div className="TokenTimeBox">
       <div className="token-time-box">
         <div className="token-name">accss_token</div>
-        <div className="token-duration">30:00</div>
+        <div className="token-duration">00:30:00</div>
       </div>
     </div>
   );
