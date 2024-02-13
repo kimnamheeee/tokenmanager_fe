26c17d94835e07be542ac4f7c70cec5abfa3c07a
sehyeon7 <worldjade07@gmail.com>
2024-02-13 17:54:31 +0900

    create: tokentime create, update logic

--- src/components/RequestBox/TokenTimeBox.jsx
+++ src/components/RequestBox/TokenTimeBox.jsx
@@ -1,4 +1,6 @@
 import "./Index.css";
+import trashbin from "../../assets/images/trash-bin.png";
+import pencil from "../../assets/images/pencil.png"
 
 const TokenTimeBox = () => {
   const handleSubmit = (e) => {
@@ -11,7 +13,15 @@ const TokenTimeBox = () => {
     <div className="TokenTimeBox">
       <div className="token-time-box">
         <div className="token-name">accss_token</div>
-        <div className="token-duration">00:30:00</div>
+        <div className="token-time-button-container">
+          <div className="token-duration">00:30:00</div>
+          <div className="delete-icon-box">
+                <img className="delete-icon" src={trashbin} width="30" />
+          </div>
+          <div className="token-time-update-button">
+                <img className="update-icon" src={pencil} width="30" />
+          </div>
+        </div>
       </div>
     </div>
   );
