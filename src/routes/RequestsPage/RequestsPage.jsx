2f6acf65804b84eb4cb47721c33c6088bfb38680
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2024-02-19 20:12:05 +0900

    feat: 시간 만료된 토큰 배열로 관리하는 로직

--- src/routes/RequestsPage/RequestsPage.jsx
+++ src/routes/RequestsPage/RequestsPage.jsx
@@ -21,6 +21,7 @@ import check from "../../assets/images/check.svg";
 
 const RequestsPage = () => {
   const [tokenTimeList, setTokenTimeList] = useState([]);
+  const [expiredTokenList, setExpiredTokenList] = useState([]);
   const [requestList, setRequestList] = useState([]);
   const [tokenList, setTokenList] = useState([]);
   const [requestIds, setRequestIds] = useState([]);
@@ -39,10 +40,18 @@ const RequestsPage = () => {
     token_name: "",
     content: "",
     request: 0,
+    expiredAt: new Date(),
   });
 
+  // useEffect(() => {
+  //   console.log("TOKEN IS EXPIRED", expiredTokenList);
+  // }, [expiredTokenList]);
+
   useEffect(() => {
-    const expiredAtList = tokenList.map((token) => token.expiredAt);
+    if (!tokenList || tokenList.some((token) => token === undefined)) return;
+    const expiredAtList = tokenList
+      .map((token) => token.expires_at)
+      .filter((expiredAt) => expiredAt !== undefined);
     setTokenExpiredAtList(expiredAtList);
   }, [tokenList]);
 
@@ -130,7 +139,6 @@ const RequestsPage = () => {
       });
       if (request) {
         const requestId = request.id;
-        const token = await createToken({ ...tokenInput, request: requestId });
         // 1. tokenTimeList에서 tokenInput.token_name과 일치하는 오브젝트 가져옴
         // 2. 그 오브젝트 내부의 .timelimit 속성을 각각 시, 분, 초로 나눈다
         // 3. new Date()를 불러온다
@@ -147,7 +155,11 @@ const RequestsPage = () => {
         createdAt.setMinutes(createdAt.getMinutes() + targetMinute);
         createdAt.setSeconds(createdAt.getSeconds() + targetSecond);
 
-        token.expiredAt = createdAt;
+        tokenInput.expiredAt = createdAt;
+        console.log("INPUT", tokenInput);
+        const token = await createToken({ ...tokenInput, request: requestId });
+
+        console.log("CREATED TOKEN", token);
 
         setTokenList([...tokenList, token]);
         setTokenInput({
@@ -268,6 +280,27 @@ const RequestsPage = () => {
   const handleAddButtonClick = () => {
     setIsAdding(true);
   };
+
+  useEffect(() => {
+    const checkExpired = setInterval(() => {
+      // 만료된 토큰을 담을 배열
+      const expiredTokens = [];
+
+      // tokenExpiredAtList를 순회하면서 만료된 토큰을 찾음
+      tokenExpiredAtList.forEach((expiredAt) => {
+        // 만료된 시간보다 현재 시간이 더 클 경우에는 만료된 토큰이므로 expiredTokens에 추가
+        if (new Date(expiredAt) < Date.now()) {
+          expiredTokens.push(expiredAt);
+        }
+      });
+
+      // 만료된 토큰을 state에 업데이트
+      setExpiredTokenList(expiredTokens);
+    }, 1000);
+
+    // 컴포넌트가 마운트 해제될 때 clearInterval로 체크하고 setInterval을 중지시킴
+    return () => clearInterval(checkExpired);
+  }, [tokenExpiredAtList]); // tokenExpiredAtList가 변경될 때마다 useEffect 실행
 
   return (
     <div className="RequestsPage">
