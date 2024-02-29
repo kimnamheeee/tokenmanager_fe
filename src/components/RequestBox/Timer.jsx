372ac619b133df434f4fe3f0d76657dfb83aa503
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2024-02-29 16:53:13 +0900

    feat: react countdown timer initial setting

--- src/components/RequestBox/Timer.jsx
+++ src/components/RequestBox/Timer.jsx
@@ -1,9 +1,19 @@
 import "./Index.css";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 const Timer = () => {
-    return(
-        <img className="Timer"/>
-    );
+  return (
+    <CountdownCircleTimer
+      isPlaying
+      duration={20}
+      colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+      colorsTime={[7, 5, 2, 0]}
+      size={38}
+      strokeWidth={5}
+    >
+      {(number) => ""}
+    </CountdownCircleTimer>
+  );
 };
 
 export default Timer;
